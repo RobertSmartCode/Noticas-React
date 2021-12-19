b279f51d2e055de6d6271f0ad299f5c2e958a27a
Robert Smart <roca1388@gmail.com>
2021-12-18 21:25:54 -0300

    Proyecto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,45 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // definir la categoria y noticias
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=31de36f3ad874070afdd28c8c47be525`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    }
+    consultarAPI();
+  }, [categoria]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <Fragment>
+        <Header 
+          titulo='Buscador de Noticias'
+        />
+
+        <div className="container white">
+            <Formulario 
+              guardarCategoria={guardarCategoria}
+            />
+
+            <ListadoNoticias 
+              noticias={noticias}
+            />
+        </div>
+    </Fragment>
+  );
 }
 
-export default App
+export default App;
+
